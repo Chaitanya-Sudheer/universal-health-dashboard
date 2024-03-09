2f29f7c2b27098b7c44c789570a320f2145fef46
Chaitanya-Sudheer <tanya2003@gmail.com>
2024-03-09 15:15:10 +0530

    resolved another conflict

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -4,10 +4,8 @@ import React from "react";
 const Body = () => {
   return (
     <div class="hero min-h-screen w-screen bg-white">
-  
   <div class="hero-content text-center text-neutral-content">
-    <div class="max-w">
-
+    <div class="max-w ">
     </div>
   </div>
 </div>

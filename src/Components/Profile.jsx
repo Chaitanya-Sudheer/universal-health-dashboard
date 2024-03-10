a4a2133e312251dc3d309fc99f6fe53430fb85d9
saira859 <sairarafi859@gmail.com>
2024-03-10 16:20:31 +0530

    profile completed

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -68,6 +68,151 @@ const Profile = () => {
               </label>
             </li>
           </ul>
+
+          <ul>
+          <li>
+              <label className="form-control w-full max-w-xs pt-0.1 pb-10 pl-10 gap-1 ml-2.5">
+                <div className="label">
+                  <span className="label-text text-black">Address</span>
+                </div>
+                <input type="text" placeholder="Type here" className="input input-bordered input-info bg-white border-2" style={{ width: '561px' }} />
+              </label>
+
+            </li>
+            <li>
+            <label className="form-control w-full max-w-xs pt-0.1 pb-10 pl-6 gap-1 ml-[640px] relative top-[-127px]">
+                <div className="label">
+                  <span className="label-text text-gray">City</span>
+                </div>
+                <div className="inline-block relative w-64">
+                  <select className="input input-bordered input-info bg-white border-2  w-full max-w-xs">
+                    <option>Kollam</option>
+                    <option>Trivandrum</option>
+                    <option>Kochi</option>
+                    <option>Thrissur</option>
+                    <option>Alappuzha</option>
+                  </select>
+                  <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                    <svg
+                      className="fill-current h-4 w-4"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                    >
+                      <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+                    </svg>
+                  </div>
+                </div>
+              </label>
+              </li>
+
+            <li>
+            <label className="form-control w-full max-w-xs pt-0.1 pb-10 pl-10 gap-1 ml-2.5 relative top-[-126px]">
+                <div className="label">
+                  <span className="label-text text-gray">State</span>
+                </div>
+                <div className="inline-block relative w-64">
+                  <select className="input input-bordered input-info bg-white border-2 w-full max-w-xs">
+                    <option>Kerala</option> 
+                    <option>Tamil Nadu</option>
+                    <option>Karnataka</option>
+                    <option>Maharashtra</option>
+                    <option>Hyderabad</option>
+                  </select>
+                  <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                    <svg
+                      className="fill-current h-4 w-4"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                    >
+                      <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+                    </svg>
+                  </div>
+                </div>
+              </label> </li>
+
+        <li>
+        <label className="form-control w-full max-w-xs pt-0.1 pb-10 pl-10 gap-1 ml-[295px] relative top-[-253px]">
+                <div className="label">
+                  <span className="label-text text-black">Zipcode</span>
+                </div>
+                <input type="text" placeholder="Type here" className="input input-bordered input-info bg-white border-2  w-full max-w-xs" />
+              </label>
+        </li>
+      
+
+      <li>
+      <label className="form-control w-full max-w-xs pt-0.1 pb-10 pl-10 gap-1 ml-[605px] relative top-[-380.5px] mr-[-57px]">
+                <div className="label">
+                  <span className="label-text text-black">Country</span>
+                </div>
+                <input type="text" placeholder="Type here" className="input input-bordered input-info bg-white border-2  w-full max-w-xs" />
+              </label>
+      </li>
+
+      <li>
+      <label className="form-control w-full max-w-xs pt-0.1 pb-10 pl-10 gap-1 ml-2.5 relative top-[-380.5px]">
+                <div className="label">
+                  <span className="label-text text-black">Phone Number</span>
+                </div>
+                <input type="text" placeholder="Type here" className="input input-bordered input-info bg-white border-2  w-full max-w-xs" />
+              </label>
+      </li>
+
+      <li>
+      <label className="form-control w-full max-w-xs pt-0.1 pb-10 pl-10 gap-1 ml-80 relative top-[-508px]">
+                <div className="label">
+                  <span className="label-text text-black">Email</span>
+                </div>
+                <input type="text" placeholder="Type here" className="input input-bordered input-info bg-white border-2  w-full max-w-xs" />
+              </label>
+
+      </li>
+      <li>
+      <label className="form-control w-full max-w-xs pt-0.1 pb-10 pl-10 gap-1 ml-2.5 relative top-[-508px]">
+                <div className="label">
+                  <span className="label-text text-black">Marital Status</span>
+                </div>
+                <input type="text" placeholder="Type here" className="input input-bordered input-info bg-white border-2  w-full max-w-xs" />
+              </label>
+      </li>
+      <li>
+      <label className="form-control w-full max-w-xs pt-0.1 pb-10 pl-10 gap-1 ml-80 relative top-[-635px]">
+                <div className="label">
+                  <span className="label-text text-black">Nationality</span>
+                </div>
+                <input type="text" placeholder="Type here" className="input input-bordered input-info bg-white border-2  w-full max-w-xs" />
+              </label></li>
+
+      <li>
+      <label className="form-control w-full max-w-xs pt-0.1 pb-10 pl-10 gap-1 ml-2.5 relative top-[-640px]">
+                <div className="label">
+                  <span className="label-text text-gray">Language Preference</span>
+                </div>
+                <div className="inline-block relative w-64">
+                  <select className="input input-bordered input-info bg-white border-2 w-full max-w-xs">
+                    <option>English</option> 
+                    <option>Hindi</option>
+                    <option>Malayalam</option>
+                    <option>Telugu</option>
+                    <option>Urdu</option>
+                  </select>
+                  <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                    <svg
+                      className="fill-current h-4 w-4"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                    >
+                      <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+                    </svg>
+                  </div>
+                </div>
+              </label>
+      </li>
+
+      
+      <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg text-white bg-blue-400 border-blue-400 hover:border-blue-500 hover:bg-blue-500 relative top-[-600px] ml-2">Submit</button>
+      
+          </ul>
         </div>
         <Dnav />
       </div>
